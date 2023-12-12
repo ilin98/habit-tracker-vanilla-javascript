a9299c2907c10138e6f79e64f427103d7a0a3ca0
Isaac <isaac.lin.pnc@gmail.com>
2023-12-12 14:30:24 -0600

    added functions for light and dark mode

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
+// Query Selectors
 const habits = document.querySelectorAll('.habit-btn');
+const themeBtn = document.querySelector('#theme');
+// FUNCTIONS
 
+const storage = {
+    saveTheme(value) {
+        localStorage.setItem('habitsapp.theme', `${value}`);
+    },
+    checkTheme(){
+        return localStorage.getItem('habitsapp.theme');
+    }
+}
 
+const ui = {
+    theme(){
+        themeBtn.classList.toggle('dark');
+        const root = document.querySelector(':root');
+        root.classList.toggle('dark');
+        themeBtn.classList.contains('dark')
+        ? storage.saveTheme('dark')
+        : storage.saveTheme('light')
+    }
+}
+
+// EVENT LISTENER
+
+// EVENT: window load
+window.addEventListener('DOMContentLoaded', () => {
+    //Load theme
+    const theme = storage.checkTheme();
+    if(theme === 'dark') ui.theme();
+})
+
+// EVENT : theme button
+themeBtn.addEventListener('click', ui.theme);
+
+// DELETE
 habits.forEach(habit => {
     habit.addEventListener('click', () => {
-        habit.classList.toggle('.completed')
-        console.log('clicked.')
+        habit.classList.toggle('completed')
     })
 })
